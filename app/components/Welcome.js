1ba8776cd7bd0a2dd42ce24039bad8f7f9491eb1
Malcolm C <55069873+malcolmjconnor@users.noreply.github.com>
2024-10-16 21:15:25 +0000

    spacing

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Welcome() {
   return (
-    <section id="home" className="h-screen flex flex-col justify-center items-center">
+    <section id="home" className="h-screen flex flex-col justify-center items-center m-4 md:m-8 lg:m-12">
       <img src="/images/mlogo.svg" alt="Logo" className="w-80 h-auto mb-4 animate-pulse transition-transform transform hover:scale-110 duration-1000" />
       <h2 className="text-3xl font-semibold text-center">
         Welcome to my{' '}
@@ -14,7 +14,7 @@ export default function Welcome() {
           </span>
         </Link>
       </h2>
-      <p className="mt-2 text-center text-gray-500">
+      <p className="mt-2 mb-4 text-center text-gray-500">
         Data analytics specialist, technology enthusiast
       </p>
     </section>
